b6a2de44eefeb16e63db55b4e1b92a60df91e627
Hojin Ghim <hojin@amazon.com>
2024-04-09 14:26:08 +0100

    Change from u7793aaa93ef05f

--- font.sh
+++ font.sh
@@ -1,8 +1,28 @@
-# https://www.marksimonson.com/fonts/view/anonymous-pro
-# via https://kinsta.com/blog/best-programming-fonts/#11-anonymous-pro
+#!env bash
+
 mkdir -p ~/.fonts
 pushd ~/.fonts
-wget https://www.marksimonson.com/assets/content/fonts/AnonymousPro-1_002.zip
-unzip AnonymousPro-1_002.zip
-ln -s AnonymousPro-1_002*/*.ttf .
+
+# https://www.marksimonson.com/fonts/view/anonymous-pro
+# via https://kinsta.com/blog/best-programming-fonts/#11-anonymous-pro
+
+#[ -f AnonymousPro-1_002.zip ] ||
+    #wget https://www.marksimonson.com/assets/content/fonts/AnonymousPro-1_002.zip
+
+# https://fonts.google.com/specimen/Nanum+Gothic+Coding?classification=Monospace&subset=korean&noto.script=Kore
+[ -f ~/Downloads/Nanum_Gothic_Coding.zip ] && mv ~/Downloads/Nanum_Gothic_Coding.zip .
+
+for name in $(basename --suffix=.zip *.zip)
+do
+    mkdir -p "$name"
+    pushd "$name"
+    unzip "../${name}.zip"
+    popd
+done
+
+find $(realpath .) -name *.ttf -type f -print0 |
+    xargs -0 -I II ln -s II .
+
 sudo fc-cache -f
+
+popd
--- gvimrc
+++ gvimrc
@@ -2,11 +2,17 @@ if has("gui_running")
     set guioptions-=T
     set guioptions-=m
     set lines=100 columns=999
+
     map <C-q> <esc>:wqa!<Enter>
+
     if has('nvim')
-        set guifont=Anonymous\ Pro:h11
+        "set guifont=Anonymous\ Pro:h11
+        "set guifont=Ubuntu\ Mono:h10
+        set guifont=NanumGothicCoding:h10
     else
-        set guifont=Anonymous\ Pro\ 11
+        "set guifont=Anonymous\ Pro\ 11
+        "set guifont=Ubuntu\ Mono\ 10
+        set guifont=NanumGothicCoding\ 10
     endif
 
 endif
