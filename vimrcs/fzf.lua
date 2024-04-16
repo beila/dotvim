842f7bea4adb318aac599ec82551d5a5edd2bf06
Hojin Ghim <hojin@amazon.com>
2024-04-16 16:26:03 +0100

    Change from u7793aaa93ef05f

--- vimrcs/fzf.lua
+++ vimrcs/fzf.lua
@@ -1,2 +1,5 @@
-require("fzf-lua").setup({ fzf_opts = {['--layout'] = 'reverse-list'} })
 require("fzf-lua").setup_fzfvim_cmds()
+require("fzf-lua").setup({ fzf_opts = {['--layout'] = 'reverse-list'}, lsp = {git_icons = true, finder = {}} })
+vim.keymap.set({ "n", "v", "i" }, "<C-]>",
+  function() require("fzf-lua").lsp_finder() end,
+  {})
