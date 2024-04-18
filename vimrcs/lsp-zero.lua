9ce459ac970ae78170833839f4ab71695730b146
Hojin Ghim <hojin@amazon.com>
2024-04-18 14:26:04 +0100

    Change from u7793aaa93ef05f

--- vimrcs/lsp-zero.lua
+++ vimrcs/lsp-zero.lua
@@ -2,20 +2,51 @@ require('lsp-zero')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    ensure_installed = {},
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
 })
 
 require('lspconfig').lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {'vim'}
-      }
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
     }
-  }
 })
+
+vim.keymap.set({ "n" }, "]a",
+    function()
+        local eo = { severity = vim.diagnostic.severity.ERROR }
+        local wo = { severity = vim.diagnostic.severity.WARN }
+        local io = { severity = vim.diagnostic.severity.INFO }
+        local ho = { severity = vim.diagnostic.severity.HINT }
+        local get = vim.diagnostic.get
+        local o = get(0, eo) and eo or get(0, wo) and wo or get(0, io) and io or ho
+        vim.diagnostic.goto_next(o)
+    end,
+    {})
+sdghi = g
+vim.keymap.set({ "n" }, "[a",
+    function()
+        local eo = { severity = vim.diagnostic.severity.ERROR }
+        vim.diagnostic.goto_prev(vim.diagnostic.get(0, eo) and eo or {})
+    end,
+    {})
+
+vim.keymap.set({ "n" }, "}A",
+    function() vim.diagnostic.goto_next() end,
+    {})
+
+vim.keymap.set({ "n" }, "{A",
+    function() vim.diagnostic.goto_prev() end,
+    {})
+
+vim.keymap.set({ "n" }, "<leader>af",
+    function() require("fzf-lua").lsp_code_actions() end,
+    {})
