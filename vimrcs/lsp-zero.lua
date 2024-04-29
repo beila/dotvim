76ade6b81495d97fbad4b41aa8f4288c25d34403
Hojin Ghim <hojin@amazon.com>
2024-04-29 16:26:03 +0100

    Change from u7793aaa93ef05f

--- vimrcs/lsp-zero.lua
+++ vimrcs/lsp-zero.lua
@@ -1,4 +1,4 @@
-require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -20,42 +20,50 @@ require('lspconfig').lua_ls.setup({
     }
 })
 
-vim.keymap.set({ "n" }, "]a",
-    function()
-        local eo = { severity = vim.diagnostic.severity.ERROR }
-        local wo = { severity = vim.diagnostic.severity.WARN }
-        local io = { severity = vim.diagnostic.severity.INFO }
-        local ho = { severity = vim.diagnostic.severity.HINT }
-        local has = function(o) return next(vim.diagnostic.get(0, o)) ~= nil end
-        local o = has(eo) and eo or has(wo) and wo or has(io) and io or ho
-        vim.diagnostic.goto_next(o)
-    end,
-    {})
+lsp_zero.on_attach(function(_, bufnr)
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        exclude = { "<F2>", "<F3>", "[d", "]d" },
+        preserve_mappings = false,
+    })
 
-vim.keymap.set({ "n" }, "[a",
-    function()
-        local eo = { severity = vim.diagnostic.severity.ERROR }
-        local wo = { severity = vim.diagnostic.severity.WARN }
-        local io = { severity = vim.diagnostic.severity.INFO }
-        local ho = { severity = vim.diagnostic.severity.HINT }
-        local has = function(o) return next(vim.diagnostic.get(0, o)) ~= nil end
-        local o = has(eo) and eo or has(wo) and wo or has(io) and io or ho
-        vim.diagnostic.goto_prev(o)
-    end,
-    {})
+    vim.keymap.set({ "n" }, "]d",
+        function()
+            local eo = { severity = vim.diagnostic.severity.ERROR }
+            local wo = { severity = vim.diagnostic.severity.WARN }
+            local io = { severity = vim.diagnostic.severity.INFO }
+            local ho = { severity = vim.diagnostic.severity.HINT }
+            local has = function(o) return next(vim.diagnostic.get(0, o)) ~= nil end
+            local o = has(eo) and eo or has(wo) and wo or has(io) and io or ho
+            vim.diagnostic.goto_next(o)
+        end,
+        {})
 
-vim.keymap.set({ "n", "v" }, "}A",
-    function() vim.diagnostic.goto_next() end,
-    {})
+    vim.keymap.set({ "n" }, "[d",
+        function()
+            local eo = { severity = vim.diagnostic.severity.ERROR }
+            local wo = { severity = vim.diagnostic.severity.WARN }
+            local io = { severity = vim.diagnostic.severity.INFO }
+            local ho = { severity = vim.diagnostic.severity.HINT }
+            local has = function(o) return next(vim.diagnostic.get(0, o)) ~= nil end
+            local o = has(eo) and eo or has(wo) and wo or has(io) and io or ho
+            vim.diagnostic.goto_prev(o)
+        end,
+        {})
 
-vim.keymap.set({ "n", "v" }, "{A",
-    function() vim.diagnostic.goto_prev() end,
-    {})
+    vim.keymap.set({ "n", "v" }, "}D",
+        function() vim.diagnostic.goto_next() end,
+        {})
 
-vim.keymap.set({ "n", "v" }, "<leader>aa",
-    function() require("fzf-lua").lsp_code_actions() end,
-    {})
+    vim.keymap.set({ "n", "v" }, "{D",
+        function() vim.diagnostic.goto_prev() end,
+        {})
 
-vim.keymap.set({ "n", "v" }, "<leader>af",
-    function() vim.lsp.buf.format() end,
-    {})
+    vim.keymap.set({ "n", "v" }, "<leader>aa",
+        function() require("fzf-lua").lsp_code_actions() end,
+        {})
+
+    vim.keymap.set({ "n", "v" }, "<leader>af",
+        function() vim.lsp.buf.format() end,
+        {})
+end)
--- vimrcs/nvim-treesitter.lua
+++ vimrcs/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "awk", "bash", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "idl", "javascript", "json", "json5", "JSON with comments", "kotlin", "lua", "make", "markdown", "markdown_inline", "passwd", "perl", "printf", "proto", "python", "regex", "pip requirements", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
+    ensure_installed = { "awk", "bash", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "idl", "javascript", "json", "json5", "kotlin", "lua", "make", "markdown", "markdown_inline", "passwd", "perl", "printf", "proto", "python", "regex", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
     auto_install = true,
     highlight = { enable = true, },
     indent={enable=true,},
