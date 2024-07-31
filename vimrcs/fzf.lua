281f2bf03fc3e7f3959054686697469b1783fae1
Hojin Ghim <hojin@amazon.com>
2024-07-31 14:26:05 +0100

    Change from u7793aaa93ef05f

--- vimrcs/fzf.lua
+++ vimrcs/fzf.lua
@@ -4,6 +4,10 @@ local fzf_lua = require("fzf-lua")
 
 fzf_lua.setup_fzfvim_cmds()
 
+vim.keymap.set({ "n", "v", "i" }, "<leader><tab>",
+    function() fzf_lua.tabs() end,
+    {})
+
 vim.keymap.set({ "n", "v", "i" }, "<F8>",
     function() fzf_lua.lsp_document_symbols() end,
     {})
@@ -14,6 +18,7 @@ vim.keymap.set({ "n", "v", "i" }, "<C-]>",
 
 fzf_lua.setup({
     fzf_opts = { ['--layout'] = 'reverse-list' },
+    oldfiles = { include_current_session = true },
     lsp = {
         git_icons = true,
         finder = { actions = { ["ctrl-]"] = { function() fzf_lua.lsp_definitions() end } } },
