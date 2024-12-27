ce793467341af83dbeff0a8b7b15017e5b309cb9
Hojin Ghim <hojin@amazon.com>
2024-12-27 13:45:04 +0000

    Change from dev-dsk-hojin-1c-1d10d9ca

--- vimrcs/fzf.lua
+++ vimrcs/fzf.lua
@@ -77,7 +77,7 @@ fzf_lua.setup({
     },
     fzf_opts = { ['--layout'] = 'reverse-list' },
     grep = {
-        rg_opts = '--follow',
+        rg_opts = '--follow --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
         rg_glob = true,
         -- first returned string is the new search query
         -- second returned string are (optional) additional rg flags
@@ -85,7 +85,7 @@ fzf_lua.setup({
         rg_glob_fn = function(query, opts)
             local regex, flags = query:match("^(.-)%s%-%-(.*)$")
             -- If no separator is detected will return the original query
-            return (regex or query), flags .. opts
+            return (regex or query), flags .. ' ' .. opts
         end,
         actions = {
             ["ctrl-r"] = { actions.toggle_ignore }
