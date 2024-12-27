8ec35c41a7c4986ca30df86586453c3f77f05b94
Hojin Ghim <hojin@amazon.com>
2024-12-27 13:50:09 +0000

    Change from dev-dsk-hojin-1c-1d10d9ca

--- vimrcs/fzf.lua
+++ vimrcs/fzf.lua
@@ -82,10 +82,10 @@ fzf_lua.setup({
         -- first returned string is the new search query
         -- second returned string are (optional) additional rg flags
         -- @return string, string?
-        rg_glob_fn = function(query, opts)
+        rg_glob_fn = function(query)
             local regex, flags = query:match("^(.-)%s%-%-(.*)$")
             -- If no separator is detected will return the original query
-            return (regex or query), flags .. ' ' .. opts
+            return (regex or query), flags
         end,
         actions = {
             ["ctrl-r"] = { actions.toggle_ignore }
