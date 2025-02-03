be3cc7584dab1d9fe3f2eaa543b099b883b7978c
Hojin Ghim <hojin@amazon.com>
2025-02-03 13:26:18 +0000

    Change from u7793aaa93ef05f

--- vimrcs/lsp-zero.lua
+++ vimrcs/lsp-zero.lua
@@ -5,7 +5,10 @@ require('mason-lspconfig').setup({
     ensure_installed = {},
     handlers = {
         function(server_name)
-            require('lspconfig')[server_name].setup({})
+            local server = require('lspconfig')[server_name]
+            if server.setup then
+                server.setup({})
+            end
         end,
     },
 })
