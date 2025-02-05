a91971508f7604d9d6cbcc89e2eccdf16cd6bd84
Hojin Ghim <hojin@amazon.com>
2025-02-05 14:21:06 +0000

    Change from u7793aaa93ef05f

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 -- https://www.reddit.com/r/neovim/comments/zfimqo/comment/izc156m
 local vimrc = vim.fn.stdpath('config') .. '/vimrc.symlink'
 vim.cmd.source(vimrc)
+
+-- https://github.com/ThePrimeagen/init.lua/blob/213009d/lua/theprimeagen/remap.lua#L23-L27
+vim.keymap.set("x", "p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_d"+P]])
+vim.keymap.set({"n", "v"}, "<leader>p", [["+P]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>Y", [["+Y]])
