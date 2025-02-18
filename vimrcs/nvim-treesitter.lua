091f6b736cc99d59b89e73b58b7f1bc18b75f3c6
Hojin Ghim <hojin@amazon.com>
2025-02-18 14:31:06 +0000

    Change from u7793aaa93ef05f

--- vimrcs/nvim-treesitter.lua
+++ vimrcs/nvim-treesitter.lua
@@ -2,5 +2,11 @@ require 'nvim-treesitter.configs'.setup {
     ensure_installed = { "awk", "bash", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "idl", "javascript", "json", "json5", "kotlin", "lua", "make", "markdown", "markdown_inline", "passwd", "perl", "printf", "proto", "python", "regex", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
     auto_install = true,
     highlight = { enable = true, },
-    indent={enable=true,},
+    indent = { enable = true, },
+    -- https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/#4-install-tree-sitter-and-set-it-up-with-the-tree-sitter-rust-parser
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    },
 }
