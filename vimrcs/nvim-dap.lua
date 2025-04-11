fe1d4c60db777774204d182bab44d027764c29d1
Hojin Ghim <hojin@amazon.com>
2025-04-11 11:43:02 +0000

    Change from dev-dsk-hojin-1c-08d25803

---
+++ vimrcs/nvim-dap.lua
@@ -0,0 +1,24 @@
+-- https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(via--codelldb)#user-content-1110-and-later
+
+local dap = require('dap')
+dap.adapters.codelldb = {
+  type = "executable",
+  command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+  -- On windows you may have to uncomment this:
+  -- detached = false,
+}
+
+local dap = require('dap')
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
