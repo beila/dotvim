62af9fc3e78b870e0f6b624fbfffb7976d1e1ec2
Hojin Ghim <hojin@amazon.com>
2025-04-15 11:51:02 +0100

    Change from u7793aaa93ef05f

--- vimrcs/nvim-dap-ui.lua
+++ vimrcs/nvim-dap-ui.lua
@@ -3,10 +3,10 @@ require("dapui").setup()
 
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
+    dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
+    dapui.open()
 end
 --[[
    [dap.listeners.before.event_terminated.dapui_config = function()
@@ -14,5 +14,5 @@ end
    [end
    ]]
 dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
+    dapui.close()
 end
